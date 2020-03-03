bdab3d9027e33c60903046c4c7fcc70b0a064287
Robert <robert.bonagura@gmail.com>
2020-03-02 19:01:26 -0500

    Add documentation to Prog2.java

--- src/Prog2.java
+++ src/Prog2.java
@@ -1,5 +1,12 @@
+/**
+ * Class used to run TuitionManager class for Program 2.
+ */
 public class Prog2
 {
+   /**
+    * Runs TuitionManager class.
+    * @param args
+    */
    public static void main(String[] args){
       TuitionManager tuitionManager = new TuitionManager();
       tuitionManager.run();
